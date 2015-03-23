8207ab331adf9afc48356ec520df86631272e533
Gunther Konig <gunther.s.konig@gmail.com>
2015-03-23 11:05:13 +0200

    implement InstallBundle subcommand

--- src/Gk/SymfonyBundleInstaller/SubCommand/InstallBundleCommand.php
+++ src/Gk/SymfonyBundleInstaller/SubCommand/InstallBundleCommand.php
@@ -4,10 +4,15 @@ namespace Gk\SymfonyBundleInstaller\SubCommand;
 
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\StringInput;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class InstallBundleCommand extends AbstractCommand
+    implements FormulaAwareInterface, ComposerApplicationAwareInterface
 {
+    use FormulaAwareTrait;
+    use ComposerApplicationAwareTrait;
+
     protected function configure()
     {
         $this->setName('install:bundle')
@@ -15,12 +20,15 @@ class InstallBundleCommand extends AbstractCommand
             ->addArgument('formula',
                 InputArgument::REQUIRED,
                 'Composer package name of the installer formula'
-            )
-        ;
+            );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $output->writeln('Running install:bundle');
+        $installerBundle = $this->getFormula()->getInstallerBundle();
+        $composerApplication = $this->getComposerApplication();
+        $composerInput = new StringInput(sprintf('require %s', $installerBundle));
+        $composerApplication->run($composerInput, $output);
     }
 }
